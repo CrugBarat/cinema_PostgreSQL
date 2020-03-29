ee5cd32c2983612652d90b2ffeb558a4c5dca0e7
Craig Barratt <craigtbarratt@gmail.com>
2020-03-29 18:59:51 +0100

    adds TDD for screen.rb

--- specs/screen_specs.rb
+++ specs/screen_specs.rb
@@ -6,4 +6,29 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestScreen < MiniTest::Test
 
+  def setup()
+
+    @screen1 = Screen.new({'name' => 'Screen 3',
+                           'capacity' => 18})
+
+  end
+
+  def test_get_name()
+    assert_equal('Screen 3', @screen1.name())
+  end
+
+  def test_get_capacity()
+    assert_equal(18, @screen1.capacity())
+  end
+
+  def test_set_name()
+    @screen1.name = 'Luxe Screen'
+    assert_equal('Luxe Screen', @screen1.name())
+  end
+
+  def test_set_capacity()
+    @screen1.capacity = 30
+    assert_equal(30, @screen1.capacity())
+  end
+
 end
